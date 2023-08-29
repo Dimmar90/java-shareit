6c218de56e6bfb1f41665be044ad773cc3787346
Dimmar90 <106697499+Dimmar90@users.noreply.github.com>
2023-08-30 02:15:16 +0300

    java-shareit

--- src/main/java/ru/practicum/shareit/exception/ServerErrorException.java
+++ src/main/java/ru/practicum/shareit/exception/ServerErrorException.java
@@ -1,6 +1,6 @@
 package ru.practicum.shareit.exception;
 
-public class ServerErrorException extends RuntimeException{
+public class ServerErrorException extends RuntimeException {
     public ServerErrorException(String message) {
         super(message);
     }
