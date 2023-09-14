ac63a14039cff879d979f14335fac0bb1a881da6
Dimmar90 <106697499+Dimmar90@users.noreply.github.com>
2023-09-14 19:26:09 +0300

    shareit

--- src/test/java/ru/practicum/shareit/ShareItTests.java
+++ src/test/java/ru/practicum/shareit/ShareItTests.java
@@ -1,29 +1,89 @@
 package ru.practicum.shareit;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 import ru.practicum.shareit.request.ItemRequest;
+import ru.practicum.shareit.request.ItemRequestController;
+import ru.practicum.shareit.request.dto.ItemRequestDto;
+import ru.practicum.shareit.request.dto.ItemRequestDtoMapper;
 import ru.practicum.shareit.request.service.RequestService;
 
-import java.sql.SQLException;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 
-@SpringBootTest
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@WebMvcTest(controllers = ItemRequestController.class)
 class ShareItTests {
 
-    @Mock
-    RequestService mockRequestService;
+    @Autowired
+    ObjectMapper mapper;
+
+    @MockBean
+    RequestService requestService;
+
+    @Autowired
+    private MockMvc mvc;
+
+    private ItemRequest itemRequest = new ItemRequest();
+
+    private ItemRequestDtoMapper requestDtoMapper = new ItemRequestDtoMapper();
+
+    @BeforeEach
+    void setRequestParameters() {
+        itemRequest.setId(1L);
+        itemRequest.setDescription("Request Description");
+    }
 
     @Test
-    void testAddItemRequest() throws SQLException {
-        ItemRequest itemRequestExpected = new ItemRequest();
-        ItemRequest itemRequest = new ItemRequest();
-
-        Mockito.when(mockRequestService.addItemRequest(1L, itemRequest))
+    void addNewItemRequestTest() throws Exception {
+        when(requestService.addItemRequest(anyLong(), any()))
                 .thenReturn(itemRequest);
 
-        Assertions.assertEquals(itemRequestExpected, mockRequestService.addItemRequest(1L, itemRequest));
+        mvc.perform(post("/requests")
+                        .header("X-Sharer-User-Id", 1)
+                        .content(mapper.writeValueAsString(itemRequest))
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated());
+
+        Assertions.assertEquals(1L, itemRequest.getId());
+        Assertions.assertEquals("Request Description", itemRequest.getDescription());
+    }
+
+    @Test
+    void findItemRequestsTest() throws Exception {
+        requestService.addItemRequest(1L, itemRequest);
+        List<ItemRequestDto> itemRequests = new ArrayList<>();
+        itemRequests.add(requestDtoMapper.toItemRequestDto(itemRequest));
+
+        when(requestService.findItemRequests(anyLong()))
+                .thenReturn(itemRequests);
+
+        mvc.perform(get("/requests")
+                        .header("X-Sharer-User-Id", 1)
+                        .content(mapper.writeValueAsString(itemRequest))
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+        Assertions.assertEquals(1L, itemRequests.get(0).getId());
+        Assertions.assertEquals("Request Description", itemRequests.get(0).getDescription());
     }
 }
